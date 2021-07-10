0248b3ab58c34fec9b8217f47be27a1ca37c6c37
dorsax <56792918+dorsax@users.noreply.github.com>
2021-07-10 17:35:25 +0200

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,1 +1,15 @@
 # this script will crawl all new data
+
+# get the config
+
+# get from database: where has the last iteration stopped? As in which page. 
+
+# get the same page again
+
+# get current max entries and calculate the max pages
+
+# if there are more pages, get the pages, too.
+
+# write all data into the database
+
+# data contains: all fetched, last page
