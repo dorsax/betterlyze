24a5d05523f945e713c531a838c4f9acae4c6e45
dorsax <56792918+dorsax@users.noreply.github.com>
2021-07-10 17:37:40 +0200

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -1,1 +1,9 @@
 # this will visualize the data previously fetched.
+
+# get the config
+
+# define the SELECT statements for the graphs
+
+# compute results and maybe write statistical tables to db
+
+# build the forms and graphs
