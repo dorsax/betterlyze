4a0524237c90188ec2e7f3c02549fbe132c3622a
dor_sax <github@dorsax.de>
2021-07-11 23:15:58 +0200

    add config

--- crawler.py
+++ crawler.py
@@ -1,6 +1,13 @@
 # this script will crawl all new data
+import yaml
+import os
 
 # get the config
+
+configstream = open(os.path.dirname(os.path.realpath(__file__))+os.path.sep+config_filename, 'r')
+config = yaml.safe_load (configstream)
+event = config.get('event')
+currentpage = config.get('lastpage',0)
 
 # get from database: where has the last iteration stopped? As in which page. 
 
