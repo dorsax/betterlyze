613102a52fec8dca4531902896deeced39460b93
dor_sax <github@dorsax.de>
2022-11-09 13:30:39 +0100

    feat: more infos on event detail page

--- betterlyze/analyse/urls.py
+++ betterlyze/analyse/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name='analyse'
 urlpatterns = [
     path('', views.compare, name='index'),
-    path('<int:event_id>/', views.detail, name='detail'),
-    path('compare/', views.compare, name='compare'),
+    path('<int:pk>/', views.EventDetail.as_view(), name='detail'),
+    path('details/', views.details, name='details'),
     path('<int:event_id>/crawl/', views.crawl, name='crawl'),
     
 ]
--- betterlyze/analyse/views.py
+++ betterlyze/analyse/views.py
@@ -1,23 +1,41 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.generic import DetailView, ListView
+from django.views.generic.detail import SingleObjectMixin
 
-from .models import Event
+from .models import Event, Donation
 from .crawler import crawl as external_crawl
 from . import event_dashboards
 #from . import event_dashboards
 # Create your views here.
 
-def index(request):
+def details(request):
     all_events_list = Event.objects.order_by('-end')
     context = {
         'all_events_list' : all_events_list,
     }
     return render(request, 'analyse/index.html', context)
 
-def detail(request, event_id):
-    event = get_object_or_404(Event, pk=event_id)
-    return render(request, 'analyse/detail.html', {'event': event})
+class DetailView (DetailView):
+    model = Event
+    template_name = 'analyse/detail.html'
+
+class EventDetail (SingleObjectMixin, ListView):
+    paginate_by = 20
+    template_name = 'analyse/event_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Event.objects.all())
+        return super(EventDetail,self).get(request, *args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super(EventDetail, self).get_context_data(**kwargs)
+        context['event'] = self.object
+        return context
+
+    def get_queryset(self):
+        return self.object.donation_set.all()
 
 def crawl(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
