efbaa7aae833f7acf290d35f7a42927f87174c80
dor_sax <github@dorsax.de>
2023-10-22 16:27:37 +0200

    fix: rendering issues with donationtable

--- betterlyze/analyse/tables.py
+++ betterlyze/analyse/tables.py
@@ -1,8 +1,17 @@
 import django_tables2 as tables
+from django.utils.html import format_html
 
 from .models import Donation
 
 class DonationTable(tables.Table):
     class Meta:
         model = Donation
-        fields = ("donated_amount_in_cents", "was_zero", "donated_at",  "donor", "message" )
+        fields = ("donated_amount_in_cents", "was_zero", "donated_at",  "donor", "message", )
+        order_by = ('donated_at', )
+
+    def render_was_zero(self, value):
+        if (value):
+            return format_html('<span class="bi bi-check"/>',)
+        else:
+            return format_html('<span class="bi bi-x"/>',)
+
