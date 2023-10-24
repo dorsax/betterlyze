5158323cbfe009ee7b6b148a728e6d69eb4ca8f0
dor_sax <github@dorsax.de>
2023-10-24 16:22:25 +0200

    chore: remove stuff from admin

--- betterlyze/analyse/admin.py
+++ betterlyze/analyse/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-
+from django.apps import apps
+from django.contrib import auth
+from django_plotly_dash import admin as dpdadmin 
 # Register your models here.
 
-from .models import Event
+# De-register all models from other apps
+# !!! Django_Cron cannot be removed by this, as it isn't registered when this script is executed. However, we need it nonetheless.
+for app_config in apps.get_app_configs():
+    for model in app_config.get_models():
+        print(model)
+        if admin.site.is_registered(model):
+            admin.site.unregister(model)
 
-admin.site.register(Event)
+# reregister Users until we rip out the admin completely
+admin.site.register(auth.models.User)
